3d4ce003676aec6ebfc720b69b6ae1f13e70c674
Tekhamian <tekhamian@gmail.com>
2020-03-21 11:02:34 -0400

    Update index.js

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,81 +1,83 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
-class NameSearch extends React.Component {
-  render(){
-    return (<p>NameSearch</p>);
-  }
-}
- export default NameSearch;
-
- class IdSearch extends React.Component {
-  render(){
-    return (<p>IdSearch</p>);
-  }
-}
- export default IdSearch;
-
-
-//  ORIGINAL CODE
-
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
+// OPTION A
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>PokeMon DataBase!</title>
+      </Head>
+      <div>
+        <p>
+          Search for Character using Name!
+        </p>
+        <Link href="/NameSearch">
+          <a>Name Search</a>
+        </Link>
       </div>
-    </main>
+      <div>
+        <p>
+          Search for Character using Id!
+        </p>
+        <Link href="/IdSearch">
+          <a>Id Search</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+export default Home;
 
-    
-  </div>
-)
 
-export default Home
+
+
+
+// OPTION C
+// import Head from 'next/head'
+
+
+// class NameSearch extends React.Component {
+//   render(){
+//     return (<p>NameSearch</p>);
+//   }
+// }
+//  export default NameSearch;
+
+//  class IdSearch extends React.Component {
+//   render(){
+//     return (<p>IdSearch</p>);
+//   }
+// }
+//  export default IdSearch;
+
+
+// OPTION B
+// const Home = () => {
+//   return (
+//     <div>
+//       <Head>
+//         <title>This is the PokeMon DataBase!</title>
+//       </Head>
+//       <div>
+//         <p>
+//           Search for Character using Name!
+//         </p>
+//         <Link href="/name">
+//           <a>Name Search</a>
+//         </Link>
+//       </div>
+//       <div>
+//         <p>
+//           Search for Character using Id!
+//         </p>
+//         <Link href="/id">
+//           <a>Id Search</a>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Home;
